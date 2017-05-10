6cf40f1cbf6ba87b660057ac89bb91772f67ea27
jaskaran1 <jaskaranvirdi1@gmail.com>
2017-05-10 15:37:01 -0700

    face annotation added

--- classification/face_classifier/openface_classifier/src/main.py
+++ classification/face_classifier/openface_classifier/src/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
+from openface_helper import OpenFaceAnotater
 
 import roslib
 
@@ -27,7 +28,9 @@ import message_filters
 
 import spencer_detected_person_association
 
-faceCascade = cv2.CascadeClassifier('/opt/ros/kinetic/share/OpenCV-3.2.0-dev/haarcascades/haarcascade_frontalface_default.xml')
+# faceCascade = cv2.CascadeClassifier('/opt/ros/kinetic/share/OpenCV-3.2.0-dev/haarcascades/haarcascade_frontalface_default.xml')
+openface_anotater = None
+# faceCascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
 
 class image_converter:
 
@@ -42,21 +45,21 @@ class image_converter:
                 , Image)
         detections_sub = \
             message_filters.Subscriber('/spencer/perception_internal/people_detection/rgbd_front_top/upper_body_detector/detections'
-                , UpperBodyDetector)
+                , UpperBodyDetector) #ROIs (the upper body rectangles)
         detected_persons_sub = \
             message_filters.Subscriber('/spencer/perception_internal/detected_persons/rgbd_front_top/upper_body'
-                , DetectedPersons)
+                , DetectedPersons)   #detection_ids corresponding to the ROIs(the ids corresponding to the rectangles)
         tracked_persons_sub = \
             message_filters.Subscriber('persons_tracked'
-                , TrackedPersons)
+                , TrackedPersons)    #Tracks
 
         self.time_sync = message_filters.ApproximateTimeSynchronizer(
             [image_sub, detections_sub, detected_persons_sub, tracked_persons_sub], 300, 0.02)
         self.time_sync.registerCallback(self.time_sync_callback)
 
-        track_sync_sub = message_filters.Subscriber('/spencer/perception_internal/detected_persons/rgbd_front_top/upper_body', DetectedPersons)
-        self.track_sync = spencer_detected_person_association.TrackSynchronizer(track_sync_sub, 300)
-        self.track_sync.registerCallback(self.track_sync_callback)
+        track_sync_sub = message_filters.Subscriber('/spencer/perception_internal/detected_persons/rgbd_front_top/upper_body', DetectedPersons) #detection_ids
+        self.track_sync = spencer_detected_person_association.TrackSynchronizer(track_sync_sub, 300) #Buffers the detection_ids till it gets tracks with same or newer timestamp
+        self.track_sync.registerCallback(self.track_sync_callback) #callback happens once a track is found with same or newer timestamp
 
     def time_sync_callback(
         self,
@@ -100,37 +103,44 @@ class image_converter:
                 h = detections.height[index]
 
                 cv_image_crop = cv_image[y:y + h, x:x + w]
-                gray = cv2.cvtColor(cv_image_crop, cv2.COLOR_BGR2GRAY)
-                faces = faceCascade.detectMultiScale(
-                    gray,
-                    scaleFactor=1.1,
-                    minNeighbors=3,
-                    minSize=(50, 50),
-                    flags = cv2.CASCADE_SCALE_IMAGE
-                )
 
-                # Draw a rectangle around the faces
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(cv_image_crop, (int(x-w*0.1), int(y-h*0.1)), (x+int(w*1.1), y+int(h*1.1)), (0, 255, 0), 2)
+                print ("cv_image_crop size is",cv_image_crop.shape)
 
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(
-                    cv_image_crop,
-                    str(track_id%10000000),
-                    (0, h),
-                    font,
-                    1*w/160.0,
-                    (255, 255, 255),
-                    2,
-                    )
+                if cv_image_crop.shape[0]>0 and cv_image_crop.shape[1]>0:
+                    cv_image_crop = cv2.cvtColor(cv_image_crop,cv2.COLOR_BGR2RGB)
+                    cv_image_crop = openface_anotater.predict(cv_image_crop, [], multiple=False, scale=None)
+                    cv_image_crop = cv2.cvtColor(cv_image_crop,cv2.COLOR_RGB2BGR)
+                else:
+                    return
+                # gray = cv2.cvtColor(cv_image_crop, cv2.COLOR_BGR2GRAY)
+                # faces = faceCascade.detectMultiScale(
+                #     gray,
+                #     scaleFactor=1.1,
+                #     minNeighbors=3,
+                #     minSize=(50, 50),
+                #     flags = cv2.CASCADE_SCALE_IMAGE
+                # )
+
+                # # Draw a rectangle around the faces
+                # for (x, y, w, h) in faces:
+                #     cv2.rectangle(cv_image_crop, (int(x-w*0.1), int(y-h*0.1)), (x+int(w*1.1), y+int(h*1.1)), (0, 255, 0), 2)
+
+                # font = cv2.FONT_HERSHEY_SIMPLEX
+                # cv2.putText(
+                #     cv_image_crop,
+                #     str(track_id%10000000),
+                #     (0, h),
+                #     font,
+                #     1*w/160.0,
+                #     (255, 255, 255),
+                #     2,
+                #     )
 
                 try:
                     self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image_crop,
                                            'bgr8'))
                 except CvBridgeError, e:
                     print(e)
-
-                break
 
     def track_sync_callback(
         self,
@@ -140,7 +150,7 @@ class image_converter:
         output = "New detections with track association available (age of detections = %.2f sec)! Detection to track association: " % age.to_sec()
 
         if detected_persons.detections:
-            tracked_persons = TrackedPersons()
+            tracked_persons = TrackedPersons() # TrackedPersons message created 
             tracked_persons.header = detected_persons.header
             for detected_person in detected_persons.detections:
                 detection_id = detected_person.detection_id
@@ -168,4 +178,5 @@ def main(args):
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
+    openface_anotater = OpenFaceAnotater(sys.argv)
     main(sys.argv)
